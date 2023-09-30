5a4f2e482fde3ff262bad8f51758da7494d6763e
hikmani <syarifulfajar017@gmail.com>
2023-09-30 11:15:06 +0700

    Config database production

--- config/database.js
+++ config/database.js
@@ -8,4 +8,11 @@ module.exports = {
     host: process.env.DB_HOST_DEV,
     dialect: "postgres",
   },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+  },
 };
