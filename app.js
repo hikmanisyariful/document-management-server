ec910fb6984d255c31d0d0082079a19622ae6522
hikmani <syarifulfajar017@gmail.com>
2023-09-30 15:41:01 +0700

    Procfile

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
+app.use(express.static(__dirname + "/public"));
+
 app.use(errorHandlare);
 
 app.listen(port, () => {
